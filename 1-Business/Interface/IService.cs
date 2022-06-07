29a42c2b98429f5320a8580a9a3ea1e65e50dc77
cinthiabs <cinthiabarbosa8d@outlook.com>
2022-06-07 16:10:05 -0300

    Envio de Fonte

---
+++ 1-Business/Interface/IService.cs
@@ -0,0 +1,16 @@
+﻿using _3_Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Business.Interface
+{
+    interface IService
+    {
+        public void ExecutaServico();
+        public List<Dados>ConsultaDados();
+        public string BuscaArquivos(Dados dados);  
+    }
+}
